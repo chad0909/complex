5b7c8ea44288c85952671a9d19b7c016c614a84c
Dong Kuen <danaver12@daum.net>
2022-07-05 15:45:04 +0900

    op_sub branch added

--- complex.py
+++ complex.py
@@ -2,7 +2,6 @@
 최동근: maintainer
 이기욱: addition,subtraction
 이혜미: multiplication
-
 '''
 class Complex:
     def __init__(self,re=0,im=0):
@@ -30,10 +29,13 @@ class Complex:
         c.im = self.im - c1.im
         return c
 
+    def __sub__(self,c): # self - c
+        return self.subtract(c)
+
 c1 = Complex(1,2)
 print(c1)
 
 c2 = Complex(2,3)
 print(c1.add(c2))
-print(c2.subtract(c1))
+print(c2-c1) # subtraction
 print(c1.multiply(c2))
